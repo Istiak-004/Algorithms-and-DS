cfc4ab5479c5727749bd48d6f6e44ecd3a3dbe98
ZeroIQ <llazydeveloper004@gmail.com>
2021-02-07 18:03:14 +0600

    data structure and algorithm

--- recursion.py
+++ recursion.py
@@ -1,6 +1,6 @@
 # how to write a recursive function for any given problem for 3 steps
 # step 1 : find the recursive flow
-#step 2 : find the condition that will handle recursive flow 
+#step 2 : find the basic conditions that will handle recursive flow 
 #step 3 : solve the recursion for all conditions/find the bug inside the code/unintensional case
 
 #find n factorial?
@@ -50,4 +50,15 @@ def fibonacchi(n):
 			a=b
 			b=c
 		return b,i
-print(fibonacchi(9))					
+print(fibonacchi(9))	
+
+
+# Q1 : find the sum of digits of a positive int using recursion?
+def sum_of_digits(n):
+	assert n>=0 and int(n)==n , 'number must be positive int'
+	if n==0:
+		return 0
+	else:
+		return int(n%10) + sum_of_digits(int(n/10))
+
+print(sum_of_digits(1234))
